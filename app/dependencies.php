31d562e388b0dd45e2b5d1fbee013b5ec35f8a2e
Arthur BLOT <arthur.blot@viacesi.fr>
2026-03-17 16:41:54 +0100

    doctrine ws

--- app/dependencies.php
+++ app/dependencies.php
@@ -4,26 +4,25 @@ declare(strict_types=1);
 
 use App\Application\Settings\SettingsInterface;
 use DI\ContainerBuilder;
+use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use Monolog\Processor\UidProcessor;
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
-// Imports manquants pour Doctrine
-use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\ORMSetup;
-use Doctrine\DBAL\DriverManager;
 use Symfony\Component\Cache\Adapter\ArrayAdapter;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 
 return function (ContainerBuilder $containerBuilder) {
     $containerBuilder->addDefinitions([
-        // DÃ©finition du Logger
         LoggerInterface::class => function (ContainerInterface $c) {
             $settings = $c->get(SettingsInterface::class);
+
             $loggerSettings = $settings->get('logger');
-            
             $logger = new Logger($loggerSettings['name']);
+
             $processor = new UidProcessor();
             $logger->pushProcessor($processor);
 
@@ -32,27 +31,27 @@ return function (ContainerBuilder $containerBuilder) {
 
             return $logger;
         },
-
-        
         EntityManager::class => function (ContainerInterface $c) {
             $settings = $c->get(SettingsInterface::class);
+            $doctrine = $settings->get('doctrine');
 
             // Use the ArrayAdapter or the FilesystemAdapter depending on the value of the 'dev_mode' setting
             // You can substitute the FilesystemAdapter for any other cache you prefer from the symfony/cache library
-            $cache = $settings['doctrine']['dev_mode'] ?
+            $cache = $doctrine['dev_mode'] ?
                 new ArrayAdapter() :
-                new FilesystemAdapter(directory: $settings['doctrine']['cache_dir']);
+                new FilesystemAdapter(directory: $doctrine['cache_dir']);
 
             $config = ORMSetup::createAttributeMetadataConfiguration(
-                $settings['doctrine']['metadata_dirs'],
-                $settings['doctrine']['dev_mode'],
+                $doctrine['metadata_dirs'],
+                $doctrine['dev_mode'],
                 null,
                 $cache
             );
 
-            $connection = DriverManager::getConnection($settings['doctrine']['connection']);
+            $connection = DriverManager::getConnection($doctrine['connection']);
 
             return new EntityManager($connection, $config);
         },
+
     ]);
 };
